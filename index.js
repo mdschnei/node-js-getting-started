576b5c4c780b95f3cfb3232c73eb18f965c55246
Matthew Schneider <matthewsschneiders@gmail.com>
2020-10-19 03:39:26 -0400

    testing api handling

--- index.js
+++ index.js
@@ -7,8 +7,11 @@ const pool = new Pool({
   }
 })
 const express = require('express')
+const bodyParser = require('body-parser');
 const path = require('path')
 const PORT = process.env.PORT || 5000
+
+var jsonParser = bodyParser.json();
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -30,3 +33,33 @@ express()
   })
 
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+
+express().post('/addRow', jsonParser, async function(request, response) {
+  console.log(request)
+  try {
+    const client = await pool.connect();
+    const result = await client.query('INSERT into test_table values (3, "dynamically added value")');
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+ 
+  
+// if ./.data/sqlite.db does not exist, create it, otherwise print records to console
+/*   if (!exists) {
+    console.log("Table not found");
+    db.run('CREATE TABLE userlist (user text, password text');
+    console.log('New table User List Created!');
+    insert(request);
+  }
+  else{
+    insert(request);
+  }
+  db.each('SELECT * from userlist', function(err, row) {
+      if ( row ) {
+        console.log('record:', JSON.stringify(row));
+      }
+    }); */
+});
---
+++ public/scripts/client.js
@@ -0,0 +1,13 @@
+// client-side js
+// run by the browser each time your view template referencing it is loaded
+
+console.log('Testing insert');
+
+function submit()
+{
+  // request the user from our app's sqlite database
+  const userRequest = new XMLHttpRequest();
+  userRequest.open('post', '/addRow');
+  userRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
+  userRequest.send(JSON.stringify({'id':document.getElementById("id").value, 'name': document.getElementById("name").value}));
+}

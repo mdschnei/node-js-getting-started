bb35d74503ab49dba6e78e17121b2b91e4ebc184
Matthew Schneider <matthewsschneiders@gmail.com>
2020-10-19 01:58:01 -0400

    testing view engines with db

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 5000
 express()
   .use(express.static(path.join(__dirname, '/public')))
   // .set('views', path.join(__dirname, 'views'))
-  // .set('view engine', 'ejs')
+  .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/db', async (req, res) => {

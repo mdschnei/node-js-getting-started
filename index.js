b117c1b885d3a5823a1c9b53ddd6c1fba0196836
Matthew Schneider <matthewsschneiders@gmail.com>
2020-10-18 21:39:57 -0400

    removed view engine from express

--- index.js
+++ index.js
@@ -11,10 +11,10 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .use(express.static(path.join(__dirname, '/public')))
+  // .set('views', path.join(__dirname, 'views'))
+  // .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('index'))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/db', async (req, res) => {
     try {
